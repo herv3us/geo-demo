22318362fce80d6c0bdd04d6bdf9869f12483459
Sofia Hervéus <sofia@herveus.se>
2021-11-15 15:18:36 +0100

    add function and little styling

--- geoscript.js
+++ geoscript.js
@@ -7,12 +7,50 @@ geoBtn.addEventListener('click', () => {
     console.log('We have a location');
 
     statusParagraph.innerHTML = 'Fetching position...';
-    navigator.geolocation.getCurrentPosition((pos) => {
-      statusParagraph.innerHTML = `Got position... Looking for adress!`;
-      console.log('Current position is: ', pos);
+    navigator.geolocation.getCurrentPosition(onSuccess, (error) => {
+      statusParagraph.innerHTML =
+        'Sorry! Something went worng.' + '<br>' + error.message;
     });
   } else {
     console.log('No location found');
     statusParagraph.innerHTML = 'No location found';
   }
 });
+
+async function onSuccess(pos) {
+  statusParagraph.innerHTML = `Got position... Looking for adress!`;
+  console.log('Current position is: ', pos);
+  const address = await lookupPosition(
+    pos.coords.latitude,
+    pos.coords.longitude
+  );
+  if (address) {
+    showAdress(address);
+  }
+}
+
+async function lookupPosition(lat, long) {
+  //   const url = `https://geocode.xyz/${lat},${long}?geoit=json`;
+  try {
+    const res = await fetch(
+      `https://geocode.xyz/${lat},${long}?geoit=json&auth=446968249876491397676x26597`
+    );
+    const data = await res.json();
+    if (data.error) {
+      statusParagraph.innerHTML =
+        "Couldn't get position." + '<br>' + error.message;
+      return null;
+    }
+    console.log(data);
+    return data;
+  } catch (error) {
+    statusParagraph.innerHTML = "Couldn't get position. " + error.message;
+    return null;
+  }
+}
+
+function showAdress(address) {
+  statusParagraph.innerHTML = `Country: ${address.country} <br> 
+City: ${address.city} <br>
+Address: ${address.staddress}`;
+}
