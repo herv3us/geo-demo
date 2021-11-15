97ceaa382d3b58c53d041007bbbb43c1fc5a0120
Sofia Hervéus <sofia@herveus.se>
2021-11-15 14:00:29 +0100

    Add HTML

--- geoscript.js
+++ geoscript.js
@@ -1,7 +1,18 @@
 console.log('Geolocation Demo');
+const geoBtn = document.querySelector('#geoBtn');
+const statusParagraph = document.querySelector('.status');
 
-if ('geolocation' in navigator) {
-  console.log('We have a location');
-} else {
-  console.log('No location found');
-}
+geoBtn.addEventListener('click', () => {
+  if ('geolocation' in navigator) {
+    console.log('We have a location');
+
+    statusParagraph.innerHTML = 'Fetching position...';
+    navigator.geolocation.getCurrentPosition((pos) => {
+      statusParagraph.innerHTML = `Got position... Looking for adress!`;
+      console.log('Current position is: ', pos);
+    });
+  } else {
+    console.log('No location found');
+    statusParagraph.innerHTML = 'No location found';
+  }
+});
